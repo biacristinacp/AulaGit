94106a09bcd30552194fd49d788419bdcdc6998b
biacristinacp <bey.cristinacp@gmail.com>
2020-03-04 12:02:34 -0300

    Repositório 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes =
+            {
+                "Beatriz Cerqueira",
+                "Gabriela Lima",
+                "Julia Miranda",
+                "Fernanda Paiva" };
+
+            foreach(var nome in nomes) { 
+                //Imprime os valores do array
+                Console.WriteLine(nome);
+            }
+            //Segura o resultado, para a tela esperando feedback do usuário
+            Console.ReadKey();
+        }
+    }
+}
+
